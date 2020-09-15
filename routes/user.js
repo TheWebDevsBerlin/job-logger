bc9da25876ef81842cffa401a895ea3482f510eb
Gilad Tsabar <gilad@tsabar.net>
2020-09-15 16:25:24 +0200

    added search box for the index page

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const User = require('../models/User');
 
 router.get('/dashboard', (req,res,next) => {
-  if(!req.user) res.redirect('/login');
+  if(!req.session.user) res.redirect('/login');
 
-  User.findOne({_id: req.user._id})
+  User.findOne({_id: req.session.user._id})
     .then(user => {
       res.render('user/dashboard', {
-        jobs: user.jobs
+        jobs: user.jobs,
+        user: req.session.user,
+        dashboard: true
     })
     .catch(err => next(err));
   });
