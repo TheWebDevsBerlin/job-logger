a8f2cca9413d394648b6057ce51377d36433666f
danp20 <dan.parry22@gmail.com>
2020-09-15 12:57:23 +0200

    commit message

--- models/User.js
+++ models/User.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  firstName: String,
+  surName: String, 
+  address: {
+    street: String,
+    city: String,
+    zip: Number,
+  },
+  jobs: [{
+    title: String,
+    company: String,
+    location: String,
+    salary: Number,
+  }],
 });
 
 const User = mongoose.model('User', userSchema);
