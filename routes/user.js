1135375a01dfca04f611d852abe942e3a0e36770
Gilad Tsabar <gilad@tsabar.net>
2020-09-15 17:59:27 +0200

    add job to user- end of the day version

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+// const Job = require('../models/Job');
+
 
 router.get('/dashboard', (req,res,next) => {
   if(!req.session.user) res.redirect('/login');
 
   User.findOne({_id: req.session.user._id})
+    .populate('jobs')
     .then(user => {
+      console.log(user);
       res.render('user/dashboard', {
         jobs: user.jobs,
         user: req.session.user,
@@ -15,6 +19,109 @@ router.get('/dashboard', (req,res,next) => {
     .catch(err => next(err));
   });
 
+});
+
+router.get('/job/add/:title/:company', (req,res,next) => {
+  if(!req.session.user) res.redirect('/login');
+console.log('ADD JOB!');
+  const {title, company} = req.params;
+
+  axios({
+    url: 'https://api.graphql.jobs/',
+    method: 'POST',
+    data: {
+      query: `
+        query job {job(input:{
+            jobSlug:"${title}"
+            companySlug:"${company}"
+          }){
+            title
+            commitment{
+              title
+              slug
+              createdAt
+              updatedAt
+            }
+            cities{
+              id
+              name
+              slug
+              country{id}
+              type
+              jobs{id}
+              createdAt
+              updatedAt
+            }
+            countries{
+              id
+              name
+              slug
+              type
+              isoCode
+              cities{id}
+              jobs{id}
+              createdAt
+              updatedAt
+            }
+            remotes{
+              id
+              name
+              slug
+              type
+              jobs{id}
+              createdAt
+              updatedAt
+            }
+            applyUrl
+            company{
+              id
+              name
+              slug
+              websiteUrl
+              logoUrl
+              jobs{id}
+              twitter
+              emailed
+              createdAt
+              updatedAt
+            }
+            tags{
+              id
+              name
+              slug
+              jobs{id}
+              createdAt
+              updatedAt
+            }
+            isPublished
+            userEmail
+            postedAt
+            createdAt
+            updatedAt
+            description
+            locationNames
+          }}
+    `}
+  })
+  .then(result => {
+    
+  });
+
+  User.findOneAndUpdate({_id: req.session.user._id})
+    .populate('jobs')
+    .then(user => {
+      axios({})
+        .then()
+        .catch(err => next(err));
+
+      console.log(user);
+      res.render('user/dashboard', {
+        jobs: user.jobs,
+        user: req.session.user,
+        dashboard: true
+    })
+    .catch(err => next(err));
+  });
 });
 
 module.exports = router;
